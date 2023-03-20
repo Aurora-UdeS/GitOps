03f2ed8efc95b23af59d4301fb5304db373b1e57
Adam B <bela1003@usherbrooke.ca>
2023-03-20 14:02:47 -0400

    add credentials file to adminsdk

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,11 @@ NC='\033[0m'
 
 echo -e "${G}pulling latest changes${NC}"
 git pull
+echo -e "${G}fetching firebase credentials${NC}"
+gcloud secrets versions access 1 --secret="firebase_secret_key" --out-file=credentials.json
 echo -e "${G}stopping docker compose${NC}"
 docker compose down
 echo -e "${G}deploying new docker compose version${NC}"
 docker compose up -d --build
+echo -e "${G}removing firebase credentials${NC}"
+rm credentials.json
