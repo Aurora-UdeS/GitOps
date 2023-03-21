74afb78d90f8dc0eaa2126710c4b078c2f5b8ed4
Adam B <bela1003@usherbrooke.ca>
2023-03-21 12:48:07 -0400

    deploy.sh in priviledge mode

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -p
 
 G='\033[0;32m'
 NC='\033[0m'
@@ -6,7 +6,7 @@ NC='\033[0m'
 echo -e "${G}pulling latest changes${NC}"
 git pull
 echo -e "${G}fetching firebase credentials${NC}"
-gcloud secrets versions access 1 --secret="firebase_secret_key" --out-file=credentials.json
+gcloud secrets versions access 1 --secret="firebase_secret_key" --out-file="credentials.json"
 echo -e "${G}stopping docker compose${NC}"
 docker compose down
 echo -e "${G}deploying new docker compose version${NC}"
