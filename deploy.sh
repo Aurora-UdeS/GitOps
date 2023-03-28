9499e9a255438cdb086e1a526ecbf726d06a76e5
Adam B <bela1003@usherbrooke.ca>
2023-03-28 11:44:52 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 echo -e "${G}pulling latest changes${NC}"
 git pull
 echo -e "${G}fetching firebase credentials${NC}"
-gcloud secrets versions access 1 --secret="firebase_secret_key" --out-file="credentials.json"
+gcloud secrets versions access 2 --secret="firebase_secret_key" > credentials.json
 echo -e "${G}insert github client id/secret in env${NC}"
 sed -i "s/GITHUB_CLIENT_ID=.*/GITHUB_CLIENT_ID=$(gcloud secrets versions access 1 --secret=github_client_id)/" .env
 sed -i "s/GITHUB_CLIENT_SECRET=.*/GITHUB_CLIENT_SECRET=$(gcloud secrets versions access 1 --secret=github_client_secret)/" .env
