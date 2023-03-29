8aa9689a59ecb9305a679887886a220b274e24c2
Adam B <bela1003@usherbrooke.ca>
2023-03-29 09:34:43 -0400

    update to take cors whitelist + add secret

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,17 @@ echo -e "${G}pulling latest changes${NC}"
 git pull
 echo -e "${G}fetching firebase credentials${NC}"
 gcloud secrets versions access 2 --secret="firebase_secret_key" > credentials.json
-echo -e "${G}insert github client id/secret in env${NC}"
-sed -i "s/GITHUB_CLIENT_ID=.*/GITHUB_CLIENT_ID=$(gcloud secrets versions access 1 --secret=github_client_id)/" .env
-sed -i "s/GITHUB_CLIENT_SECRET=.*/GITHUB_CLIENT_SECRET=$(gcloud secrets versions access 1 --secret=github_client_secret)/" .env
+echo -e "${G}fetch and insert github client id/secret in env${NC}"
+sed -i "s/GITHUB_CLIENT_ID=.*/GITHUB_CLIENT_ID=$(gcloud secrets versions access 1 --secret="github_client_id")/" .env
+sed -i "s/GITHUB_CLIENT_SECRET=.*/GITHUB_CLIENT_SECRET=$(gcloud secrets versions access 1 --secret="github_client_secret")/" .env
+echo -e "${G}fetch and insert postgres secret in env${NC}"
+sed -i "s/POSTGRES_USER=.*/POSTGRES_USER=$(gcloud secrets versions access 1 --secret="postgres_user")/" .env
+sed -i "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$(gcloud secrets versions access 1 --secret="postgres_password")/" .env
 echo -e "${G}stopping docker compose${NC}"
 docker compose down
 echo -e "${G}deploying new docker compose version${NC}"
 docker compose up -d --build
 echo -e "${G}removing firebase credentials${NC}"
-rm credentials.json
+rm -rf credentials.json
 echo -e "${G}clean env${NC}"
 git checkout -- .env
