89bb527846fe67a0ec14720a65dd9194bcce3641
Adam B <bela1003@usherbrooke.ca>
2023-04-03 19:14:50 -0400

    test

--- node/esbuild.js
+++
@@ -1,9 +0,0 @@
-import esbuild from 'esbuild';
-
-esbuild.build({
-  entryPoints: ['app.js'],
-  outfile: 'app.cjs',
-  external: ['pg-native'], // add 'pg-native' to the list of external modules
-  bundle: true,
-  platform: 'node'
-}).catch(() => process.exit(1));
