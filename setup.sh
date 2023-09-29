679b10b6f5c55cd5ab72f57927067b335fb03956
Adam B <bela1003@usherbrooke.ca>
2023-09-29 11:15:41 -0400

    remove apikey validation in nginx

--- setup.sh
+++ setup.sh
@@ -5,13 +5,6 @@ NC='\033[0m'
 
 echo -e "${G}pulling latest changes${NC}"
 git pull
-echo -e "${G}setup nginx/map.conf${NC}"
-cat <<EOF > nginx/map.conf
-map \$http_apikey \$api_client_name {
-    default       "";
-    "b28d443c-12a8-49b7-ab51-94fft027564d"  "frontend";
-}
-EOF
 echo -e "${G}setup gcloud${NC}"
 gcloud auth login
 gcloud auth configure-docker northamerica-northeast1-docker.pkg.dev
