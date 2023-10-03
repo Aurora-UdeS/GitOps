039564bec8ca9db213949122ab53aeb1c557ad09
Adam B <bela1003@usherbrooke.ca>
2023-10-03 13:12:34 -0400

    changes for AUR-668 (data persistency)

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,21 @@
 G='\033[0;32m'
 NC='\033[0m'
 
+function cleanup() {
+    echo -e "${G}removing firebase credentials${NC}"
+    rm -rf credentials.json
+    echo -e "${G}clean env${NC}"
+    git checkout -- .env
+    exit 1
+}
+
 echo -e "${G}pulling latest changes${NC}"
 git pull
 echo -e "${G}fetching adminsdk credentials${NC}"
 gcloud secrets versions access 3 --secret=adminsdk_secret_key > credentials.json
+
+trap cleanup SIGINT SIGTERM
+
 echo -e "${G}fetch and insert github client id/secret in env${NC}"
 sed -i "s/GITHUB_CLIENT_ID=.*/GITHUB_CLIENT_ID=$(gcloud secrets versions access 1 --secret=github_client_id)/" .env
 sed -i "s/GITHUB_CLIENT_SECRET=.*/GITHUB_CLIENT_SECRET=$(gcloud secrets versions access 1 --secret=github_client_secret)/" .env
@@ -19,16 +30,13 @@ sed -i "s/EMAIL_PASSWORD=.*/EMAIL_PASSWORD=$(gcloud secrets versions access 1 --
 echo -e "${G}fetch and insert coinmarketcap secret in env${NC}"
 sed -i "s/COINMARKETCAP_API_KEY=.*/COINMARKETCAP_API_KEY=$(gcloud secrets versions access 1 --secret=coinmarketcap_api_key)/" .env
 echo -e "${G}setup NGINX port${NC}"
-if [ "$1" = "dev" ]; then
-    sed -i "s/NGINX_PORT=.*/NGINX_PORT=80/" .env
-elif [ "$1" = "local" ]; then
+if [ "$1" = "local" ]; then
     sed -i "s/NGINX_PORT=.*/NGINX_PORT=5174/" .env
+else
+    sed -i "s/NGINX_PORT=.*/NGINX_PORT=80/" .env
 fi
 echo -e "${G}stopping docker compose${NC}"
 docker compose down
 echo -e "${G}deploying new docker compose version${NC}"
 docker compose up -d --build
-echo -e "${G}removing firebase credentials${NC}"
-rm -rf credentials.json
-echo -e "${G}clean env${NC}"
-git checkout -- .env
+
