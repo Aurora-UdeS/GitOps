5179fa35c709c6b3fee970b1520eb90e5df2dc34
Adam B <bela1003@usherbrooke.ca>
2023-10-05 14:13:58 -0400

    include fonts has assets

--- node/node_esbuild.js
+++ node/node_esbuild.js
@@ -1,9 +1,30 @@
 import esbuild from 'esbuild';
 
-esbuild.build({
-  entryPoints: ['app.js'],
-  outfile: 'app.cjs',
-  external: ['pg-native'], // add 'pg-native' to the list of external modules
-  bundle: true,
-  platform: 'node'
+
+
+const esbuild = require('esbuild');
+const fs = require('fs');
+
+// Define the path to Helvetica.afm for the current service
+const helveticaAfmPath = './node_modules/pdfkit/js/data/Helvetica.afm';
+const helveticaBoldAfmPath = './node_modules/pdfkit/js/data/Helvetica-Bold.afm';
+
+// Check if the file exists before adding it as an asset
+if (fs.existsSync(helveticaAfmPath) && fs.existsSync(helveticaBoldAfmPath)) {
+  esbuild.build({
+    entryPoints: ['app.js'],
+    outfile: 'app.cjs',
+    external: ['pg-native'], // add 'pg-native' to the list of external modules
+    bundle: true,
+    platform: 'node',
+    assets: [helveticaAfmPath, helveticaBoldAfmPath],
+  }).catch(() => process.exit(1));
+} else {
+  esbuild.build({
+    entryPoints: ['app.js'],
+    outfile: 'app.cjs',
+    external: ['pg-native'], // add 'pg-native' to the list of external modules
+    bundle: true,
+    platform: 'node'
 }).catch(() => process.exit(1));
+}
