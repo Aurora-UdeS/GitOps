4e6c3ddbb54df449d4f2de2d4c50d0b6669a1bf4
Adam B <bela1003@usherbrooke.ca>
2023-10-05 14:28:10 -0400

    try using file loader on .afm font files

--- node/node_esbuild.js
+++ node/node_esbuild.js
@@ -1,26 +1,10 @@
 import esbuild from 'esbuild';
-import fs from 'fs';
 
-// Define the path to Helvetica.afm for the current service
-const helveticaAfmPath = './node_modules/pdfkit/js/data/Helvetica.afm';
-const helveticaBoldAfmPath = './node_modules/pdfkit/js/data/Helvetica-Bold.afm';
-
-// Check if the file exists before adding it as an asset
-if (fs.existsSync(helveticaAfmPath) && fs.existsSync(helveticaBoldAfmPath)) {
-  esbuild.build({
-    entryPoints: ['app.js'],
-    outfile: 'app.cjs',
-    external: ['pg-native'], // add 'pg-native' to the list of external modules
-    bundle: true,
-    platform: 'node',
-    assets: [helveticaAfmPath, helveticaBoldAfmPath],
-  }).catch(() => process.exit(1));
-} else {
-  esbuild.build({
-    entryPoints: ['app.js'],
-    outfile: 'app.cjs',
-    external: ['pg-native'], // add 'pg-native' to the list of external modules
-    bundle: true,
-    platform: 'node'
+esbuild.build({
+  entryPoints: ['app.js'],
+  outfile: 'app.cjs',
+  external: ['pg-native'], // add 'pg-native' to the list of external modules
+  bundle: true,
+  platform: 'node',
+  loader: { '.afm': 'file' },
 }).catch(() => process.exit(1));
-}
