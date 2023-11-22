5c305c2af14e6c9d281d090095604b65517d6f0e
Cedric Charron <chac0902@usherbrooke.ca>
2023-11-22 15:48:09 -0500

    Corrected Reviewer for test user timesheets

--- sql/clear.sql
+++ sql/clear.sql
@@ -42,5 +42,5 @@ VALUES
 INSERT INTO review ( id, report_id, comments, reviewer_id, updated_at, updated_by, is_timesheet)
 VALUES
 ('a47a287f-2e6c-4699-a3e1-70b0c712ce78','2e77b4d7-1be8-41a7-ae16-c3999d2a1565', 'no comments', 'dYvmmHAgIbOqpXjrrBMd4UA8yAB2', CURRENT_TIMESTAMP, 'f2f071e8-95bf-11ed-a1eb-0242ac120002', TRUE),
-('b47a287f-2e6c-4699-a3e1-70b0c712ce79','2d13b44d-6998-4235-9ceb-93ef73dc0466', 'no comments', 'ZRFiYaPe1lgaRUMOuMPOYvfywfi1', CURRENT_TIMESTAMP, 'f2f071e8-95bf-11ed-a1eb-0242ac120002', TRUE),
-('c47a287f-2e6c-4699-a3e1-70b0c712ce80','1304012f-5c4b-494b-97e1-4b26d0dc8767', 'no comments', 'Sx4ukvieqcZsU7pyhl23eRvSsoi1', CURRENT_TIMESTAMP, 'f2f071e8-95bf-11ed-a1eb-0242ac120002', TRUE);
+('b47a287f-2e6c-4699-a3e1-70b0c712ce79','2d13b44d-6998-4235-9ceb-93ef73dc0466', 'no comments', 'dYvmmHAgIbOqpXjrrBMd4UA8yAB2', CURRENT_TIMESTAMP, 'f2f071e8-95bf-11ed-a1eb-0242ac120002', TRUE),
+('c47a287f-2e6c-4699-a3e1-70b0c712ce80','1304012f-5c4b-494b-97e1-4b26d0dc8767', 'no comments', 'ZRFiYaPe1lgaRUMOuMPOYvfywfi1', CURRENT_TIMESTAMP, 'f2f071e8-95bf-11ed-a1eb-0242ac120002', TRUE);
