f5467d4f9d6fa5fed37f027cad5a977d5466891e
Cedric Charron <chac0902@usherbrooke.ca>
2023-11-22 14:58:21 -0500

    Updated Insert to match table version

--- sql/clear.sql
+++ sql/clear.sql
@@ -33,8 +33,8 @@ WHERE employee_id IN ('dYvmmHAgIbOqpXjrrBMd4UA8yAB2', 'ZRFiYaPe1lgaRUMOuMPOYvfyw
 DELETE FROM report
 WHERE employee_id IN ('dYvmmHAgIbOqpXjrrBMd4UA8yAB2', 'ZRFiYaPe1lgaRUMOuMPOYvfywfi1', 'Sx4ukvieqcZsU7pyhl23eRvSsoi1' );
 
-INSERT INTO timesheet
+INSERT INTO timesheet(id, employee_id, status, payed, start_date, end_date) 
 VALUES
-  ('2e77b4d7-1be8-41a7-ae16-c3999d2a1545', 'dYvmmHAgIbOqpXjrrBMd4UA8yAB2', 'draft','FALSE', '2023-01-01', null, null, null, 0, 0, 0, null),
-  ('2d13b44d-6998-4235-9ceb-93ef73dc0446', 'ZRFiYaPe1lgaRUMOuMPOYvfywfi1', 'draft','FALSE', '2023-01-01', null, null, null, 0, 0, 0, null),
-  ('1304012f-5c4b-494b-97e1-4b26d0dc8747', 'Sx4ukvieqcZsU7pyhl23eRvSsoi1', 'draft','FALSE', '2023-01-01', null, null, null, 0, 0, 0, null);
+  ('2e77b4d7-1be8-41a7-ae16-c3999d2a1565', 'dYvmmHAgIbOqpXjrrBMd4UA8yAB2', 'draft','FALSE', '2023-01-01', null),
+  ('2d13b44d-6998-4235-9ceb-93ef73dc0466', 'ZRFiYaPe1lgaRUMOuMPOYvfywfi1', 'draft','FALSE', '2023-01-01', null),
+  ('1304012f-5c4b-494b-97e1-4b26d0dc8767', 'Sx4ukvieqcZsU7pyhl23eRvSsoi1', 'draft','FALSE', '2023-01-01', null);
